8db228a56f3d3798d12fd51af5606a188c54d38a
tugberkugurlu <tugberkugurlu@gmail.com>
2013-08-04 21:57:44 +0300

    added youtube sample

---
+++ Integrations/YoutubeExtractorsample/YoutubeExtractorsample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YoutubeExtractor;
+
+namespace YoutubeExtractorsample {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            // Our test youtube link
+            string link = "http://www.youtube.com/watch?v=3O3-yDmhEcU";
+            IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(link);
+
+            Console.ReadLine();
+        }
+    }
+}
