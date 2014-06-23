37bd53ef2c97b4de7f842ead0b400706d917d1f2
tugberkugurlu <tugberkugurlu@gmail.com>
2014-06-23 23:14:15 +0300

    added a comment

--- DotNetSpecific/LotteryMath/LotteryMath/Program.cs
+++ DotNetSpecific/LotteryMath/LotteryMath/Program.cs
@@ -3,13 +3,13 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LotteryMath
 {
     class Program
     {
+        // Read: Generating permutations of a set (most efficiently) - http://stackoverflow.com/questions/11208446
+
         private readonly static ConcurrentBag<LotteryCoupon> Bag = new ConcurrentBag<LotteryCoupon>();
         private readonly static IEnumerable<int> AllAvailableNumbers = Enumerable.Range(1, 64).ToList();
 
@@ -44,6 +44,58 @@ namespace LotteryMath
             {
                 numbers.Add(num);
             }
+        }
+
+        private static bool NextPermutation(int[] numList)
+        {
+            /*
+             Taken from http://stackoverflow.com/a/11208543/463785
+             
+             Knuths
+             1. Find the largest index j such that a[j] < a[j + 1]. If no such index exists, the permutation is the last permutation.
+             2. Find the largest index l such that a[j] < a[l]. Since j + 1 is such an index, l is well defined and satisfies j < l.
+             3. Swap a[j] with a[l].
+             4. Reverse the sequence from a[j + 1] up to and including the final element a[n].
+
+             */
+
+            int largestIndex = -1;
+            for (var i = numList.Length - 2; i >= 0; i--)
+            {
+                if (numList[i] < numList[i + 1])
+                {
+                    largestIndex = i;
+                    break;
+                }
+            }
+
+            if (largestIndex < 0)
+            {
+                return false;
+            }
+
+            int largestIndex2 = -1;
+            for (int i = numList.Length - 1; i >= 0; i--)
+            {
+                if (numList[largestIndex] < numList[i])
+                {
+                    largestIndex2 = i;
+                    break;
+                }
+            }
+
+            int tmp = numList[largestIndex];
+            numList[largestIndex] = numList[largestIndex2];
+            numList[largestIndex2] = tmp;
+
+            for (int i = largestIndex + 1, j = numList.Length - 1; i < j; i++, j--)
+            {
+                tmp = numList[i];
+                numList[i] = numList[j];
+                numList[j] = tmp;
+            }
+
+            return true;
         }
     }
 
@@ -123,6 +175,8 @@ namespace LotteryMath
 
         public override string ToString()
         {
+            // TODO: This is incorrect. Fix this later...
+
             return _first.ToString(CultureInfo.InvariantCulture) +
                    _second.ToString(CultureInfo.InvariantCulture) +
                    _third.ToString(CultureInfo.InvariantCulture) +
