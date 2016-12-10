07d8b43a3d217186c2fcc7ef436c30d7697fb8c4
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2016-12-10 12:24:16 +0000

    added DNS and IP sample

---
+++ DotNetSpecific/DNSIPAddress/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string potentialIPAddress = "tugberkugurlu.com";
+
+            var ipAddress = Dns.GetHostEntryAsync(potentialIPAddress).Result.AddressList.Last();
+            Console.WriteLine(string.Join(",", hostEntry.AddressList.Select(x => x.ToString())));
+
+            IPAddress ipAddress;
+            if(IPAddress.TryParse(potentialIPAddress, out ipAddress)) 
+            {
+                Console.WriteLine($"ip address: {ipAddress}");
+            }
+            else 
+            {
+                System.Console.WriteLine("not an ip address, resolving ip address...");
+            }
+        }
+    }
+}
