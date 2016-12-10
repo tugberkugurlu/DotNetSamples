a0fbd0ed63b2fd932b0c51f98cee43ccdb9409e1
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2016-12-10 12:24:28 +0000

    added Rx .NET Core Sample

---
+++ DotNetSpecific/RxSample-NetCore/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reactive.Subjects;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Comment out the sample you would like to run
+
+            HelloWorld();
+            // RunObserversSync().Wait();
+            // RunObserversAsync().Wait();
+
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        {
+            using(var subject = new Subject<int>())
+            {
+                // this will be fired to nowhere as there are no consumers
+                subject.OnNext(42);
+
+                var s1 = subject.Subscribe(x => Console.WriteLine($"s1> {x}"));
+
+                // s1 will consume this
+                subject.OnNext(43);
+
+                var s2 = subject.Subscribe(x => Console.WriteLine($"s2> {x}"));
+
+                // s1 and s2 will consume this
+                subject.OnNext(44);
+                s1.Dispose();
+
+                // s2 will consume this
+                subject.OnNext(45);
+
+                s2.Dispose();
+            }
+        }
+    }
+}
